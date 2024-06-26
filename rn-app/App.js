eeab6ad26c3907aaa18939ac89372bb21b2f1cdd
Script-Craftr <ennbadger@st.ug.edu.gh>
2024-06-26 07:27:19 -1200

    edited entry point and installed necessary packages

---
+++ rn-app/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  
+
+  return (
+    <View style={styles.container}>
+
+      
+      </View>
+
+
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#EBEAED',
+  },
+  
+});
